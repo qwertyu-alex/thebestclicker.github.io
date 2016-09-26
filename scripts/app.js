ba85a894ba70e39f0f7c74d0855438b7a2e9b0af
Alexander Van Le <alexandervanleprivat@gmail.com>
2016-09-26 15:43:49 +0200

    v2

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
   if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-      alert("mobile")
-      /*
+
       var height = $(document).height()
       var width = $(document).width()
       var picWidth = Math.floor(width/2)
@@ -38,7 +37,7 @@ $(document).ready(function() {
 
       animate();
       bass()
-*/
+
   } else {
 
   let body = document.body;
